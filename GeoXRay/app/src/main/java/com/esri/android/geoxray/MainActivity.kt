0d482da9ef7825fcb5f001c1fd77fe8ec4147de1
gunt0001 <gheppner@esri.com>
2019-01-14 16:43:46 +0000

    depth indicators, measure tool

--- GeoXRay/app/src/main/java/com/esri/android/geoxray/MainActivity.kt
+++ GeoXRay/app/src/main/java/com/esri/android/geoxray/MainActivity.kt
@@ -3,6 +3,7 @@ package com.esri.android.geoxray
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -10,20 +11,36 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
+import com.esri.arcgisruntime.UnitSystem
+import com.esri.arcgisruntime.data.QueryParameters
 import com.esri.arcgisruntime.data.ServiceFeatureTable
+import com.esri.arcgisruntime.geoanalysis.LocationDistanceMeasurement
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.PointCollection
+import com.esri.arcgisruntime.geometry.Polygon
+import com.esri.arcgisruntime.geometry.Polyline
 import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.mapping.ArcGISScene
 import com.esri.arcgisruntime.mapping.view.*
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.Session
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
 import kotlinx.android.synthetic.main.activity_main.*
-
-
+import kotlinx.android.synthetic.main.distance_info.*
+import java.text.DecimalFormat
+import java.util.*
+import java.util.concurrent.ExecutionException
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mDepthGuideSymbol: SimpleLineSymbol
+    private lateinit var mSurfacePointSymbol: SimpleMarkerSymbol
+
     private val LOCATION_PERMISSION_CODE = 10
 
     private var mUserRequestedInstall = true
@@ -144,6 +161,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setUpARScene(location: Location) {
+
+        mDepthGuideSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.DASH, Color.LTGRAY, 1f)
+        mSurfacePointSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.RED, 5f)
+
         // Create scene without a basemap.  Background for scene content provided by device camera.
         sceneView.setScene(ArcGISScene());
 
@@ -159,17 +180,29 @@ class MainActivity : AppCompatActivity() {
         sandstoneFeatureLayer.sceneProperties.surfacePlacement = LayerSceneProperties.SurfacePlacement.RELATIVE
         opLayers.add(sandstoneFeatureLayer)
 
+        val graphicsOverlay0 = GraphicsOverlay()
+        sceneView.graphicsOverlays.add(graphicsOverlay0)
+        drawDepthGuidesFor(sandstoneFeatureTable, graphicsOverlay0)
+
         val mudstoneFeatureTable = ServiceFeatureTable(
                 "https://services1.arcgis.com/6677msI40mnLuuLr/arcgis/rest/services/GeoXRay_WFL1/FeatureServer/1")
         val mudstoneFeatureLayer = FeatureLayer(mudstoneFeatureTable)
         mudstoneFeatureLayer.sceneProperties.surfacePlacement = LayerSceneProperties.SurfacePlacement.RELATIVE
         opLayers.add(mudstoneFeatureLayer)
 
+        val graphicsOverlay1 = GraphicsOverlay()
+        sceneView.graphicsOverlays.add(graphicsOverlay1)
+        drawDepthGuidesFor(mudstoneFeatureTable, graphicsOverlay1)
+
         val basaltFeatureTable = ServiceFeatureTable(
                 "https://services1.arcgis.com/6677msI40mnLuuLr/arcgis/rest/services/GeoXRay_WFL1/FeatureServer/2")
         val basaltFeatureLayer = FeatureLayer(basaltFeatureTable)
         basaltFeatureLayer.sceneProperties.surfacePlacement = LayerSceneProperties.SurfacePlacement.RELATIVE
         opLayers.add(basaltFeatureLayer)
+
+        val graphicsOverlay2 = GraphicsOverlay()
+        sceneView.graphicsOverlays.add(graphicsOverlay2)
+        drawDepthGuidesFor(basaltFeatureTable, graphicsOverlay2)
 
         val boreholeFeatureTable = ServiceFeatureTable(
                 "https://services1.arcgis.com/6677msI40mnLuuLr/arcgis/rest/services/GeoXRay_WFL1/FeatureServer/3")
@@ -205,10 +238,119 @@ class MainActivity : AppCompatActivity() {
         val arMotionSource = ARCoreMotionDataSource(arSceneView,this);
         fpcController.setDeviceMotionDataSource(arMotionSource);
         fpcController.setFramerate(FirstPersonCameraController.FirstPersonFramerate.BALANCED);
-        sceneView.setCameraController(fpcController);
+        sceneView.setCameraController(fpcController);val distanceMeasurement = LocationDistanceMeasurement(Point(0.0, 0.0, 0.0),
+                Point(1.0, 1.0, 1.0))
+        distanceMeasurement.unitSystem = UnitSystem.METRIC
+        val currDistanceTextView = findViewById<TextView>(R.id.currDistanceTextView)
+        val twoDecPlaces = DecimalFormat("##.00")
+        distanceMeasurement.addMeasurementChangedListener { measurementChangedEvent ->
+            currDistanceTextView.text = twoDecPlaces.format(distanceMeasurement.directDistance.value).toString()
+        }
+        val analysisOverlay = AnalysisOverlay()
+        analysisOverlay.isVisible = false
+        sceneView.analysisOverlays.add(analysisOverlay)
+        analysisOverlay.analyses.add(distanceMeasurement)
+
+        val width = resources.displayMetrics.widthPixels
+        val height = resources.displayMetrics.heightPixels
+
+        val centerScreenPoint = android.graphics.Point(width / 2, height / 2)
+
+        // set up distance measurement
+        startDistanceButton.setOnClickListener { v ->
+
+            // distance measurement task
+            val distanceTimerTask = object : TimerTask() {
+                override fun run() {
+                    val cameraPoint = fpcController.initialPosition.location
+                    val sightLinePoints = ArrayList<Point>()
+                    sightLinePoints.add(Point(cameraPoint.x, cameraPoint.y, cameraPoint.z))
+                    //Log.d(TAG, "Camera point: " + cameraPoint.getX() + ", " + cameraPoint.getY() + ", " + cameraPoint.getZ());
+                    val lookAtPointFuture = sceneView!!.screenToLocationAsync(centerScreenPoint)
+                    lookAtPointFuture.addDoneListener {
+                        try {
+                            val lookAtPoint = lookAtPointFuture.get()
+                            sightLinePoints.add(Point(lookAtPoint.x, lookAtPoint.y, lookAtPoint.z))
+                            //Log.d(TAG, "Look at point: " + lookAtPoint.getX() + ", " + lookAtPoint.getY() + ", " + lookAtPoint.getZ());
+
+                            // update distance measurement start and end points
+                            distanceMeasurement.startLocation = cameraPoint //TODO - BUG! This doesn't update!
+                            distanceMeasurement.endLocation = lookAtPoint
+
+                        } catch (e: InterruptedException) {
+                            Log.e("MapView", "Error getting point on surface: " + e.message)
+                        } catch (e: ExecutionException) {
+                            Log.e("MapView", "Error getting point on surface: " + e.message)
+                        }
+                    }
+                }
+            }
+            val distanceTimer = Timer()
+            distanceTimer.scheduleAtFixedRate(distanceTimerTask, 50, 50)
+
+            // identify task
+            val identifyTimerTask = object : TimerTask() {
+                override fun run() {
+                    val identifyFuture = sceneView!!
+                            .identifyLayersAsync(centerScreenPoint, 10.0, false)
+                    identifyFuture.addDoneListener {
+                        try {
+                            val identifyLayerResultList = identifyFuture.get()
+                            if (!identifyLayerResultList.isEmpty()) {
+                                val identifyLayerResult = identifyLayerResultList[identifyLayerResultList.size - 1]
+                                val layerName = identifyLayerResult.layerContent.name
+
+                                runOnUiThread { currLayerTextView.text = layerName }
+                            }
+                        } catch (e: InterruptedException) {
+                            Log.e("MapView", "Error in identify: " + e.message)
+                        } catch (e: ExecutionException) {
+                            Log.e("MapView", "Error in identify: " + e.message)
+                        }
+                    }
+                }
+            }
+            val identifyTimer = Timer()
+            identifyTimer.scheduleAtFixedRate(identifyTimerTask, 2000, 2000)
+        }
+
 
         // To update position and orientation of the camera with device sensors use:
         arMotionSource.startAll();
+    }
+
+    private fun drawDepthGuidesFor(serviceFeatureTable: ServiceFeatureTable, graphicsOverlay: GraphicsOverlay) {
+
+        // get all features in the scene view's spatial reference
+        val parameters = QueryParameters()
+        parameters.whereClause = "1=1"
+        parameters.outSpatialReference = sceneView.spatialReference
+        val sandstoneResult = serviceFeatureTable.queryFeaturesAsync(parameters)
+        sandstoneResult.addDoneListener {
+            try {
+                for (feature in sandstoneResult.get()) {
+                    val triangle = feature.geometry as Polygon
+                    val triangleParts = triangle.parts
+                    for (point in triangleParts.partsAsPoints) {
+                        // create a point at the surface
+                        val pointOnSurface = Point(point.x, point.y, 141.0)
+                        val depthGuidePoints = ArrayList<Point>()
+                        depthGuidePoints.add(point)
+                        depthGuidePoints.add(pointOnSurface)
+                        val surfacePointGraphic = Graphic(pointOnSurface, mSurfacePointSymbol!!)
+                        graphicsOverlay.graphics.add(surfacePointGraphic)
+                        graphicsOverlay.sceneProperties.surfacePlacement = LayerSceneProperties.SurfacePlacement.RELATIVE
+                        val depthGuidePointPair = PointCollection(depthGuidePoints)
+                        val depthGuidePolyline = Polyline(depthGuidePointPair)
+                        graphicsOverlay.graphics.add(Graphic(depthGuidePolyline, mDepthGuideSymbol!!))
+                    }
+                }
+            } catch (e: InterruptedException) {
+                Log.e("MapView", "Error querying features: " + e.message)
+            } catch (e: ExecutionException) {
+                Log.e("MapView", "Error querying features: " + e.message)
+            }
+        }
     }
 
 }
